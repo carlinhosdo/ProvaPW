cd4b79723c605de546165b0cd0045d954374cc85
kaiols <107064486+kaiols@users.noreply.github.com>
2022-06-13 07:03:08 -0300

    Add files via upload

---
+++ view/informacoes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escolinha Cesar Surf</title>
+    <link rel="stylesheet" href="informacoes.css">
+    <div style="text-align:center">
+</head>
+<body>
+       <div class="informacoes" >
+             <button id="voltar" style= > Voltar </button > 
+            <h1> Informações </h1> <Br>
+            <input type="radio" name="radioInformacoes" id="iniciante"> <label> Iniciante </label> <br > 
+            <input type="radio" name="radioInformacoes" id="amador">  <label> Amador </label> <br > 
+            <input type="radio" name="radioInformacoes" id="profissional">  <label> Profissional </label> <br > 
+            <div class="informacoes-botoes"> <br> <br>
+                <button id="agendamento"> Agendamento de prancha</button > 
+                <button id="doacoes" > Doações </button > 
+                <button id="campeonatos" > Campeonatos </button > 
+            </div>
+        </div>
+</body>
+</html>
