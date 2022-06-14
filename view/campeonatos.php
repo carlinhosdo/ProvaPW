2510413d1c6a345d57d7a1f70df0dd9b7c853744
cintiaws <106750692+cintiaws@users.noreply.github.com>
2022-06-14 14:08:48 -0300

    Add files via upload

---
+++ view/campeonatos.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Escolinha Cesar Surf </title>
+    <link rel="stylesheet" href="campeonatos.css">
+</head>
+<body>
+    <div class="Caixa" >
+            <button id="btnvoltar" > Voltar </button>
+    </div>
+
+        <h1 id="titulocamp" > Campeonato </h1>
+
+    <div class="informacoes" >
+        <h2 id="nome" > Nome </h2>
+            <input type="text"  id="cadastro" placeholder=" Faça seu cadastro" required >
+         <h2 id="nome"> Modalidades </h2>
+            <input type="text"  id="modalidades" placeholder=" Faça seu cadastro" required >
+        <div class="butao" > <br>
+            <button id="btncadastro"> Cadastrar </button>
+            <button id="btnvt"> Ver Tudo</button>
+    </div>
+    
+    <div class="premiacoes" >
+                <h1 id="premiacoes"> Premiações <h1>
+                    <h2 id="profissional" > Profissional </h2>
+                        <label for=" prprimeiro "> 1° Lugar - R$ 33 Milhões </label > <br >
+                       <label for=" prsegunddo "> 2° Lugar - R$ 31,3 Milhões </label > <br > 
+                        <label for=" prterceiro "> 3° Lugar - R$ 29,7 Milhões </label > <br >
+    </div>
+    
+    <div class="Amador" > 
+                <h2 id="amador" > Amador </h2>
+                    <label for=" amprimeiro" > 1° Lugar - R$ 22 Mil </label > <br >
+                    <label for=" amsegundo ">  2° Lugar - R$ 12 Mil </label > <br >
+                    <label for=" amterceiro ">  3° Lugar - R$ 3 Mil </label > <br >
+    </div>
+
+    </body>
+    </html>
---
+++ view/informacoes.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escolinha Cesar Surf</title>
+    <div style="text-align:center">
+    <link rel="stylesheet" href="informacoes.css">
+</head>
+<body>
+    <div class="container">
+        <div class="informacoes" >
+    <button id="voltar" style= > Voltar </button > 
+            <h1> Informações </h1> <Br>
+            <input type="radio" name="radioInformacoes" id="iniciante"> <label> Iniciante </label> <br > 
+            <input type="radio" name="radioInformacoes" id="amador">  <label> Amador </label> <br > 
+            <input type="radio" name="radioInformacoes" id="profissional">  <label> Profissional </label> <br > 
+            <div class="informacoes-botoes"> <br> <br>
+                <button id="agendamento"> Agendamento de prancha</button > 
+                <button id="doacoes" > Doações </button > 
+                <button id="campeonatos" > Campeonatos </button > 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
