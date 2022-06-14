74c9fe084bf195e91f068e39590a05137069bb17
cintiaws <106750692+cintiaws@users.noreply.github.com>
2022-06-14 15:52:17 -0300

    Add files via upload

--- view/campeonatos.php
+++ view/campeonatos.php
@@ -9,35 +9,40 @@
 </head>
 <body>
     <div class="Caixa" >
-            <button id="btnvoltar" > Voltar </button>
-    </div>
-
-        <h1 id="titulocamp" > Campeonato </h1>
+        <div class="topnavbar">
+                    <button id="btnvoltar" > Voltar </button>
+                    <h1 id="titulocamp" > Campeonato </h1>
+            </div>
+        </div>
 
     <div class="informacoes" >
-        <h2 id="nome" > Nome </h2>
+        <div class="informacoes-texto">
+            <h2 id="nome" > Nome </h2>
             <input type="text"  id="cadastro" placeholder=" Adicione o seu nome" required >
-         <h2 id="nome"> Modalidades </h2>
+            <h2 id="nome"> Modalidades </h2>
             <input type="text"  id="modalidades" placeholder="Sua modalidade" required >
-        <div class="butao" > <br>
-            <button id="btncadastro"> Cadastrar </button>
-            <button id="btnvt"> Ver Tudo</button>
+            <div class="butao" > <br>
+                <button id="btncadastro"> Cadastrar </button>
+                <button id="btnvt"> Ver Tudo</button>
+            </div>
+        </div>
+        <div class="premiacao-container">
+            <div class="profissional">
+                <h2>Profissional</h2>
+                <p>1º lugar - R$ 33 Milhões</p>
+                <p>2° Lugar - R$ 31,3 Milhões</p>
+                <p>3° Lugar - R$ 29,7 Milhões</p>
+            </div>
+            <div class="amador">
+                <h2>Amador</h2>
+                <p>1° Lugar - R$ 22 Mil</p>
+                <p>2° Lugar - R$ 12 Mil</p>
+                <p>3° Lugar - R$ 3 Mil</p>
+            </div>
+        </div>
     </div>
     
-    <div class="premiacoes" >
-                <h1 id="premiacoes"> Premiações <h1>
-                    <h2 id="profissional" > Profissional </h2>
-                        <label for=" prprimeiro "> 1° Lugar - R$ 33 Milhões </label > <br >
-                       <label for=" prsegunddo "> 2° Lugar - R$ 31,3 Milhões </label > <br > 
-                        <label for=" prterceiro "> 3° Lugar - R$ 29,7 Milhões </label > <br >
-    </div >
     
-    <div class="Amador" > 
-                <h2 id="amador" > Amador </h2>
-                    <label for=" amprimeiro" > 1° Lugar - R$ 22 Mil </label > <br >
-                    <label for=" amsegundo ">  2° Lugar - R$ 12 Mil </label > <br >
-                    <label for=" amterceiro ">  3° Lugar - R$ 3 Mil </label > <br >
-    </div>
 
     </body>
     </html>
