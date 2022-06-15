16e7039d17bb2f83d5ec267d866e344599673a50
carlinhosdo <96793917+carlinhosdo@users.noreply.github.com>
2022-06-15 08:58:51 -0300

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-        <div class="login" >
-            <h1> Confirmação de Idade </h1 >
-            <input type="text" placeholder=" Nome " >
-            <br >
-            <input type="text" placeholder="insira sua idade " >
-            <br >
-            <button> Enviar </button >
-        </div >
