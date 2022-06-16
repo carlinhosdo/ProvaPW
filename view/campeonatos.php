e8d904948e3543a7e69c4147a84656c693368503
cintiaws <106750692+cintiaws@users.noreply.github.com>
2022-06-16 15:05:17 -0300

    Add files via upload

--- view/campeonatos.php
+++ view/campeonatos.php
@@ -1,37 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Massa</title>
+    <link rel="stylesheet" href="campeonatos.css">
+    <link rel="stylesheet" href="style/alertify.css">
+</head>
+<body>
 <div class="Caixa" >
     <div class="topnavbar">
                 <button id="btnvoltar" > Voltar </button>
                 <h1 id="titulocamp" > Campeonato </h1>
+                <button id="btnvt"> Ver Tudo</button>
         </div>
     </div>
 
 <div class="campeonato-informacoes" >
     <div class="informacoes-texto">
-        <h2 id="nome" > Nome </h2>
-        <input type="text"  id="cadastro" placeholder=" Adicione o seu nome" required >
-        <h2 id="nome"> Modalidades </h2>
-        <input type="text"  id="modalidades" placeholder="Sua modalidade" required >
-        <div class="butao" > <br>
-            <button id="btncadastro"> Cadastrar </button>
-            <button id="btnvt"> Ver Tudo</button>
-        </div>
+         <h2 id="nome" > Nome </h2> <br>
+         <input type="text"  id="inputNome" placeholder=" Adicione o seu nome" required> 
+         <h2 id="nomeModalidades"> <br> Modalidades </h2> <br>
+         <input type="text"  id="inputModalidade" placeholder="Sua modalidade" required> 
+         <div class="butao" >  <br>
+            <button id="cadastrarCampeonato"> Cadastrar </button>
+         </div>
     </div>
     <div class="premiacao-container">
-        <div class="profissional">
-            <h2>Profissional</h2>
-            <p>1º lugar - R$ 33 Milhões</p>
-            <p>2° Lugar - R$ 31,3 Milhões</p>
-            <p>3° Lugar - R$ 29,7 Milhões</p>
+        <div class="premiacoes">
+            <h2>Profissional</h2> <br>
+            <p>1º lugar - R$ 33 Milhões</p> <br>
+            <p>2° Lugar - R$ 31,3 Milhões</p> <br>
+            <p>3° Lugar - R$ 29,7 Milhões</p> <br>
         </div>
-        <div class="amador">
-            <h2>Amador</h2>
-            <p>1° Lugar - R$ 22 Mil</p>
-            <p>2° Lugar - R$ 12 Mil</p>
-            <p>3° Lugar - R$ 3 Mil</p>
+        <div class="Amador">
+            <h2>Amador</h2> <br>
+            <p>1° Lugar - R$ 22 Mil</p> <br>
+            <p>2° Lugar - R$ 12 Mil</p> <br>
+            <p>3° Lugar - R$ 3 Mil</p> <br>
         </div>
     </div>
 </div>
+    <script src="script/jquery.js"></script>
+    <script src="script/app.js"></script>
+    <script src="script/alertify.js"></script>
+</body>
+</html>
 
-
-<script src="script/jquery.js"></script>
-<script src="script/app.js"></script>
